241a47b6fec822081a75f7c381d5dc879c5ed71a
Philip Mateescu <github@philipmat.net>
2014-04-25 17:09:43 -0500

    support for .dot-file, test and verbose parameters

--- install.sh
+++ install.sh
@@ -1,23 +1,51 @@
 #!/usr/bin/env bash
+TEST=false
+VERBOSE=false
+
+for i in "$@" ; do
+	case $i in
+		"-t"|"--test")
+			TEST=true
+			;;
+		"-v"|"--verbose")
+			VERBOSE=true
+			;;
+		*)
+			;;
+	esac
+	shift
+done
 
 function linking_me_softly() {
 	source=$1
 	dest=$2
-	echo "Link: $1 => $2"
+	[[ $VERBOSE == true || $TEST == true ]] && echo "Link: $1 => $2"
 }
 
 
 for directory in $(ls -d */) ; do
-	if [[ -f ${directory}.dot ]] ; then
-		linking_me_softly ${directory%%/} $HOME/.${directory%%/}
+	dir=${directory%%/}
+	[[ $VERBOSE == true ]] && echo "Handling $dir:"
+	if [[ -f $dir/.dot ]] ; then
+		[[ $VERBOSE == true ]] && echo "Has .dot; will link into $HOME"
+		linking_me_softly $dir $HOME/.$dir
 	fi
-	for dotfile 
-	for dotfile in ${directory}.??* ; do
+	for dotfile in $dir/.dot-* ; do
+		[[ ! -f $dotfile ]] && continue
+		basedot=$(basename $dotfile)
+		newfile=${basedot##.dot-}
+		dir=$(dirname $dotfile)
+		[[ $VERBOSE == true ]] && echo "Has $basedot; will link $dir into $HOME/$newfile"
+		linking_me_softly $dir $HOME/$newfile
+	done
+	for dotfile in $dir/.??* ; do
+		[[ ! -f $dotfile ]] && continue
 		basedot=$(basename $dotfile)
 		[[ $basedot == '.dot' || $basedot == .dot-* ]] && continue
+		[[ $VERBOSE == true ]] && echo "$basedot is dot-file; will link into $HOME/$basedot"
 		linking_me_softly $dotfile $HOME/$basedot
 	done
-	if [[ -s ${directory}_install.sh ]] ; then
+	if [[ -s $dir/_install.sh ]] ; then
 		cd $directory
 		source _install.sh
 		cd ..
