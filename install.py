da5b216521eb661f746eb8641320dee84f5be795
Philip Mateescu <github@philipmat.net>
2016-04-30 02:15:27 -0500

    added usage. first mention of update mode

--- install.py
+++ install.py
@@ -10,17 +10,17 @@ PLATFORM = sys.platform # one of
 # Mac OS X	'darwin'
 VERBOSE = False
 TEST = False
+UPDATE = False
 
 def get_apps():
-    """Returns all the directories at the same level as install.py"""
+    """Returns all 'apps', that is all the directories at the same level as install.py"""
     pass
 
 def install_app(app):
     pass
     
 def link(source, target):
-    """
-    Creates a link from source to target.
+    """Creates a link from source to target.
     
     Is aware of operating system."""
     if os.name == 'posix':
@@ -35,6 +35,26 @@ if __name__ == '__main__':
     args = sys.argv[1:]
     VERBOSE = ('--verbose' in args) or ('-v' in args)
     TEST = ('--test' in args) or ('-t' in args)
+    UPDATE = ('--update' in args) or ('-u' in args) or ('update' in args)
+    usage = ('--help' in args) or ('-h' in args) or ('/?' in args)
     
-    for app in get_apps():
-        install_app(app)
+    if usage:
+        print("""
+python install.py [OPTIONS]
+
+OPTIONS:
+  -v
+  --verbose  - Details all the steps it takes.
+  -t
+  --test     - Does not actually perform any operation, only pretends.
+  -u
+  --update   - UPDATE MODE, two-way update of .dotfiles
+
+UPDATE MODE:
+Update mode will attempt to pull the remote branch, 
+merge it into current and if successful, will also 
+attempt to push your local changes to remote.
+        """)
+    else:
+        for app in get_apps():
+            install_app(app)
