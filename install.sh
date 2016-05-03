f437005f5e1fcd7d31c33ce49d95bd44ae809e3d
Philip Mateescu <github@philipmat.net>
2016-05-03 08:17:01 -0500

    A more complicated install script that proper accounts for _install.sh files.

--- install.sh
+++ install.sh
@@ -35,28 +35,40 @@ function linking_me_softly() {
 for directory in $(ls -d */) ; do
 	dir=${directory%%/}
 	[[ $VERBOSE == true ]] && echo "Handling $dir:"
-	if [[ -f $dir/.dot ]] ; then
-		[[ $VERBOSE == true ]] && echo "Has .dot; will link into $HOME"
-		linking_me_softly $dir $HOME/.$dir
-	fi
-	for dotfile in $dir/.dot-* ; do
-		[[ ! -f $dotfile ]] && continue
-		basedot=$(basename $dotfile)
-		newfile=${basedot##.dot-}
-		dir=$(dirname $dotfile)
-		[[ $VERBOSE == true ]] && echo "Has $basedot; will link $dir into $HOME/$newfile"
-		linking_me_softly $dir $HOME/$newfile
-	done
-	for dotfile in $dir/.??* ; do
-		[[ ! -f $dotfile ]] && continue
-		basedot=$(basename $dotfile)
-		[[ $basedot == '.dot' || $basedot == .dot-* ]] && continue
-		[[ $VERBOSE == true ]] && echo "$basedot is dot-file; will link into $HOME/$basedot"
-		linking_me_softly $dotfile $HOME/$basedot
-	done
 	if [[ -s $dir/_install.sh ]] ; then
 		cd $directory
 		source _install.sh
+		# this should import a _dotfiles_install_$directory function into the space
+		basedot=$(basename $directory)
+		install_func="_dotfiles_install_$basedot"
+		[[ $VERBOSE == true ]] && echo "Installing according to $dir/_install.sh::$install_func."
+		install_to=$(get_abs_filename $dir)
+		[[ $(type -t $install_func) == 'function' ]] && eval $install_func $install_to $HOME
+		unset install_func
 		cd ..
+	else
+		has_dot=false
+		if [[ -f $dir/.dot ]] ; then
+			has_dot=true
+			[[ $VERBOSE == true ]] && echo "Has .dot; will link into $HOME"
+			linking_me_softly $dir $HOME/.$dir
+		fi
+		for dotfile in $dir/.dot-* ; do
+			has_dot=true
+			[[ ! -f $dotfile ]] && continue
+			basedot=$(basename $dotfile)
+			newfile=${basedot##.dot-}
+			dir=$(dirname $dotfile)
+			[[ $VERBOSE == true ]] && echo "Has $basedot; will link $dir into $HOME/$newfile"
+			linking_me_softly $dir $HOME/$newfile
+		done
+		for dotfile in $dir/.??* ; do
+			[[ ! -f $dotfile ]] && continue
+			basedot=$(basename $dotfile)
+			[[ $basedot == '.dot' || $basedot == .dot-* ]] && continue
+			[[ $VERBOSE == true ]] && echo "$basedot is dot-file; will link into $HOME/$basedot"
+			linking_me_softly $dotfile $HOME/$basedot
+		done
+		
 	fi
 done
--- vim/_install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# current folder should be vim
-source=$(pwd -P)
-# linking_me_softly "$source" "$HOME/.vim"
