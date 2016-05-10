4b45d166f46ec55b59c5d7b5d85d2717836b0569
Philip Mateescu <github@philipmat.net>
2016-05-09 20:59:12 -0500

    new function style

--- tmux/_install.sh
+++ tmux/_install.sh
@@ -1,1 +1,6 @@
-linking_me_softly .tmux $HOME/.tmux
+function _dotfiles_install_tmux() {
+    local dot_path="$1"
+    local target="$2"
+    # linking_me_softly .tmux $HOME/.tmux
+    linking_me_softly "$dot_path/tmux/.tmux" "$target/.tmux"
+}
