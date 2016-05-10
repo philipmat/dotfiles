da6ce55d329b1a4aed7303e7c93783b199e79b0a
Philip Mateescu <github@philipmat.net>
2016-05-10 14:15:05 +0000

    commenting out some debugging info

--- git/_install.sh
+++ git/_install.sh
@@ -2,7 +2,7 @@ function _dotfiles_install_git() {
     local dot_path="$1"  
     local target="$2"
 
-	echo "GIT 1=$1, 2=$2"
+	# echo "GIT 1=$1, 2=$2"
     linking_me_softly "$dot_path/.gitconfig" "$target/.gitconfig"
     linking_me_softly "$dot_path/.gitignore_global" "$target/.gitignore_global"
     linking_me_softly "$dot_path/.git-prompt.sh" "$target/.git-prompt.sh"
