90837ea4d2df02db069cde253e3c45fa6b238a67
Philip Mateescu <github@philipmat.net>
2016-05-19 10:29:44 -0500

    allowing for a local.ps1 file for machine-specific settings

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,16 @@
 
 
 # Import-Module "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\PowerShell\Modules\SQLPS\SQLPS.PS1" 
-Import-Module PsGet
+Import-Module -ErrorAction:Ignore PsGet
 
-Import-Module PSake
+Import-Module -ErrorAction:Ignore PSake
 
 if(!(Test-Path Alias:\psake)){ 
 	New-Alias psake Invoke-PSake 
 }
 function poop { Invoke-PSake gutcheck }
 
-Import-Module posh-git
+Import-Module -ErrorAction:Ignore posh-git
 function prompt {
 	Write-Host -NoNewline -ForegroundColor Gray "[ "
 	Write-Host -NoNewline -ForegroundColor White "$pwd"
@@ -39,33 +39,26 @@ function global:prompt {
 }
 
 
-Function Reload-Module($ModuleName)
-{
-
-	if((get-module -list | where{$_.name -eq "$ModuleName"} | measure-object).count -gt 0)
-	{
-
-		if((get-module -all | where{$_.Name -eq "$ModuleName"} | measure-object).count -gt 0)
-		{
+Function Reload-Module($ModuleName) {
+	if((get-module -list | where{$_.name -eq "$ModuleName"} | measure-object).count -gt 0) {
+		if((get-module -all | where{$_.Name -eq "$ModuleName"} | measure-object).count -gt 0) {
 			rmo $ModuleName
-				Write-Host "Module $ModuleName Unloading"
+			Write-Host "Module $ModuleName Unloading"
 		}
-
 		ipmo $ModuleName
-			Write-Host "Module $ModuleName Loaded"
+		Write-Host "Module $ModuleName Loaded"
 	}
-	Else
-	{
+	Else {
 		Write-Host "Module $ModuleName Doesn't Exist"
 	}
-
 }
+
 Function n() {} # this is to fix the multiple Ns when coming out of git unlink index failed issue
 New-Alias -Name rlo -Value Reload-Module
 
 Enable-GitColors
 
-Import-Module PowerYaml
+Import-Module -ErrorAction:Ignore PowerYaml
 
 New-Alias d Get-ChildItem 
 # New-Alias cat Get-Content
@@ -73,4 +66,8 @@ New-Alias g git
 New-Alias p Invoke-PSake
 
 
-Set-Location C:\dev\Estream\Source
+# any per-machine settings can be set in a local.ps1 file
+$localps1 = Join-Path $PSScriptRoot 'local.ps1'
+if((Test-Path $localps1)){ 
+	. $localps1
+}
