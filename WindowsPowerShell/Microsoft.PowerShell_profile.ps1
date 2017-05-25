1bc43c90cb86c76e8cde20e64123036621ff96b6
Philip Mateescu <github@philipm.at>
2017-05-25 17:50:34 -0500

    correct update for tab autocompletion

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,3 @@
-
-
 # Import-Module "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\PowerShell\Modules\SQLPS\SQLPS.PS1" 
 Import-Module -ErrorAction:Ignore PsGet
 
@@ -57,32 +55,26 @@ Function n() {} # this is to fix the multiple Ns when coming out of git unlink i
 New-Alias -Name rlo -Value Reload-Module
 
 if ($gitStatus) {
-	# Enable-GitColors
-	Start-SshAgent -Quiet
-	# Set up tab expansion and include psake expansion
-		<############### Start psakeTabExpansion Initialization Code
-#########################>
-		Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition
-				-Parent)
-		. ./Modules/psake/tabexpansion/PsakeTabExpansion.ps1
-		Pop-Location
-		if((Test-Path Function:\TabExpansion) -and (-not (Test-Path
-						Function:\DefaultTabExpansion))) {
-			Rename-Item Function:\TabExpansion DefaultTabExpansion
-		}
-	function TabExpansion($line, $lastWord) {
-		$lastBlock = [regex]::Split($line, '[|;]')[-1]
-			switch -regex ($lastBlock) {
-# Execute psake tab completion for all psake-related
-				commands
-					'(Invoke-psake|psake) (.*)' { PsakeTabExpansion $lastBlock
-					}
-# Fall back on existing tab expansion
-				default { DefaultTabExpansion $line $lastWord }
-			}
-	}
-	<############### End psakeTabExpansion Initialization Code
-###########################>
+    # Enable-GitColors
+    Start-SshAgent -Quiet
+    # Set up tab expansion and include psake expansion
+    ############### Start psakeTabExpansion Initialization Code #########################
+    Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition-Parent)
+    . ./Modules/psake/tabexpansion/PsakeTabExpansion.ps1
+    Pop-Location
+    if ((Test-Path Function:\TabExpansion) -and (-not (Test-Path Function:\DefaultTabExpansion))) {
+        Rename-Item Function:\TabExpansion DefaultTabExpansion
+    }
+    function TabExpansion($line, $lastWord) {
+        $lastBlock = [regex]::Split($line, '[|;]')[-1]
+        switch -regex ($lastBlock) {
+            # Execute psake tab completion for all psake-related commands
+            '(Invoke-psake|psake) (.*)' { PsakeTabExpansion $lastBlock }
+            # Fall back on existing tab expansion
+            default { DefaultTabExpansion $line $lastWord }
+        }
+    }
+    ############### End psakeTabExpansion Initialization Code ###########################
 }
 
 Import-Module -ErrorAction:Ignore PowerYaml
