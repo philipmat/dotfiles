464e406ca243b4896c1387180648d5c7a3edff18
Philip Mateescu <github@philipm.at>
2023-06-11 19:36:29 -0500

    Creates .config directory

--- nvim/_install.sh
+++ nvim/_install.sh
@@ -1,6 +1,6 @@
 function _dotfiles_install_nvim() {
     local dot_path="$1" # typically this folder
     local target="$2" # typically $HOME
-
+    mkdir -p "$HOME/.config"
     linking_me_softly "$dot_path" "$HOME/.config/nvim"
 }
