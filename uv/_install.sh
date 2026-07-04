7f112ba6a5a564d8bb00eda92389d0b76716cb04
Philip M <github@philipm.at>
2026-07-04 18:07:59 -0500

    no separate install for uv

--- uv/_install.sh
+++
@@ -1,6 +0,0 @@
-function _dotfiles_install_nvim() {
-    local dot_path="$1" # typically this folder
-    local target="$2" # typically $HOME
-    mkdir -p "$HOME/.config"
-    linking_me_softly "$dot_path" "$HOME/.config/uv"
-}
